c1e873a40b2eaa0acd71d31b12bfa01dff0cc2e0
Rania Yqb <raniayaqoubi1995@gmail.com>
2022-07-24 19:07:00 +0200

    adding forms

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,12 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Candidate;
+use App\Entity\Recruiter;
+use App\Entity\User;
+use App\Form\CandidateType;
+use App\Form\RecruiterType;
+use App\Repository\CandidateRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class UserController extends AbstractController
 {
+    private $security;
+
     #[Route('/profil', name: 'app_user')]
     public function index(): Response
     {
@@ -17,10 +28,48 @@ class UserController extends AbstractController
     }
 
     #[Route('/profil/completer-profil', name: 'app_user_complete_profile')]
-    public function completeProfile(): Response
+    public function completeProfile(Request $request, EntityManagerInterface $entityManager): Response
     {
+        if ($this->isGranted('ROLE_CANDIDATE')){
+            $candidate = new Candidate();
+
+            $form = $this->createForm(CandidateType::class, $candidate);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $candidate->setUser($this->getUser());
+                $entityManager->persist($candidate);
+                $entityManager->flush();
+
+                $user = $this->getUser();
+                $user->setCandidate($candidate);
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_user');
+            };
+
+
+
+
+
+        } elseif ($this->isGranted('ROLE_RECRUITER')) {
+            $recruiter = new Recruiter();
+
+            $form = $this->createForm(RecruiterType::class, $recruiter);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $entityManager->persist($recruiter);
+                $entityManager->flush();
+            };
+        };
+
         return $this->render('user/user_profile_fill.html.twig', [
-            'controller_name' => 'UserController',
+            'profileFillForm' => $form->createView(),
         ]);
     }
 }
--- src/Form/CandidateType.php
+++ src/Form/CandidateType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Candidate;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +13,8 @@ class CandidateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('lastname')
-            ->add('firstname')
-            ->add('resume')
+            ->add('lastname', TextType::class)
+            ->add('firstname', TextType::class)
         ;
     }
 
